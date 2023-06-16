0f0fa9f5b70196b1b3a6c7b34ca3844d9d19a175
Robert Mitchell <rbmv@roberts-air.lan>
2023-06-16 10:04:15 -0700

    this needed to be updated

--- create-structure.sh
+++ create-structure.sh
@@ -1,78 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
-# make sure we're ~
-cd ~/
+# make it verbose
+set -x
 
-# --> main file structure
-mkdir code cloud media tmp
+# </> create code directories
+mkdir -p $HOME/code/{personal, professional, work}
+mkdir -p $HOME/code/work/{skylight, dph, dhs, srht}
 
-# --> code
-cd ~/code/
-mkdir bin projects
+# --> clone some repos
+gh repo clone robertmitchellv/robertmitchellv.github.io $HOME/code/professional/
+gh repo clone robertmitchellv/talks $HOME/code/professional/
+gh repo clone CDCgov/phdi $HOME/code/work/skylight/
+gh repo clone CDCgov/phdi-azure $HOME/code/work/skylight/
+gh repo clone CDCgov/phdi-site $HOME/code/work/skylight/
 
-## --> bin
-cd ~/code/bin/
-mkdir javascript Python R
-
-### --> javascript
-cd ~/code/bin/javascript/
-mkdir docs npm repos
-
-### --> python
-cd ~/code/bin/python/
-mkdir libraries enviornments
-
-### --> R
-cd ~/code/bin/R/
-mkdir docs packages repos
-
-## --> projects
-cd ~/code/projects/
-mkdir personal professional work
-
-### --> work
-cd ~/code/projects/work/
-mkdir srht
-
-# --> cloud
-# Insall Box, Dropbox, OneDrive, etc
-
-# --> media
-cd ~/media/
-mkdir assets audio game image text video
-
-### --> assets
-cd ~/media/assets/
-mkdir typefaces
-
-### --> audio
-cd ~/media/audio/
-mkdir audiobook music podcast-interview
-
-### --> image
-cd ~/media/image/
-mkdir personal art wallpaper
-
-### --> text
-cd ~/media/text/
-mkdir books documents
-
-#### --> books
-cd ~/media/text/books
-mkdir literature non-fiction technical
-
-#### --> documents
-cd ~/media/text/documents
-mkdir created obtained
-
-### --> video
-cd ~/media/video
-mkdir personal tv-movie
-
-# --> get path ready for homebrew
-echo "export LC_ALL=en_US.UTF-8" >> ~/.bash_profile
-echo "export LANG=en_US.UTF-8" >> ~/.bash_profile
-echo "export PATH=/usr/local/bin:$PATH" >> ~/.bash_profile && source ~/.bash_profile
-
-# --> clone vundle for vim bundles
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
