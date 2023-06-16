8607f36d272aec3eb53912f1056a9027e69a67f1
Robert Mitchell <rbmv@roberts-air.lan>
2023-06-16 10:11:17 -0700

    typo with repo names in target directories

--- create-structure.sh
+++ create-structure.sh
@@ -3,14 +3,16 @@
 # make it verbose
 set -x
 
+# </> create temp for home
+mkdir $HOME/tmp
+
 # </> create code directories
-mkdir -p $HOME/code/{personal, professional, work}
-mkdir -p $HOME/code/work/{skylight, dph, dhs, srht}
+mkdir -p $HOME/code/{personal,professional,work}
+mkdir -p $HOME/code/work/{skylight,dph,dhs,srht}
 
 # --> clone some repos
-gh repo clone robertmitchellv/robertmitchellv.github.io $HOME/code/professional/
-gh repo clone robertmitchellv/talks $HOME/code/professional/
-gh repo clone CDCgov/phdi $HOME/code/work/skylight/
-gh repo clone CDCgov/phdi-azure $HOME/code/work/skylight/
-gh repo clone CDCgov/phdi-site $HOME/code/work/skylight/
-
+gh repo clone robertmitchellv/robertmitchellv.github.io $HOME/code/professional/robertmitchellv.github.io
+gh repo clone robertmitchellv/talks $HOME/code/professional/talks
+gh repo clone CDCgov/phdi $HOME/code/work/skylight/phdi
+gh repo clone CDCgov/phdi-azure $HOME/code/work/skylight/phdi-azure
+gh repo clone CDCgov/phdi-site $HOME/code/work/skylight/phdi-site
